65b6757df1b496ac5d9ed8b860ac22e2d8b6f10b
kkkkh <haotian.zhang@seaboxdata.com>
2022-03-10 16:16:24 +0800

    test 6 package 3

--- src/components/vue-transfer-pagination/index.js
+++ src/components/vue-transfer-pagination/index.js
@@ -1,7 +1,8 @@
-"use strict";
-exports.__esModule = true;
-var index_vue_1 = require("./index.vue");
-var install_1 = require("../../utitls/install");
-var install = install_1.installCon(index_vue_1["default"]).install;
-index_vue_1["default"].install = install;
-exports["default"] = index_vue_1["default"];
+import VueTransferPagination from "./main.vue";
+
+/* istanbul ignore next */
+VueTransferPagination.install = function(Vue) {
+  Vue.component(VueTransferPagination.name, VueTransferPagination);
+};
+
+export default VueTransferPagination;
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./plugins/element.js";
+import VueTransferPagination from "vue-transfer-pagination";
+
 Vue.use(VueCompositionAPI);
 Vue.config.productionTip = false;
+Vue.use(VueTransferPagination);
 
 new Vue({
   router,
