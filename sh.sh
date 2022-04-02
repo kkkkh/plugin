29fb9078e706187b8ae05f7e91b9af95bf9f065c
kkkkh <haotian.zhang@seaboxdata.com>
2022-04-02 18:58:58 +0800

    fix @vue/composition-api package bug

--- sh.sh
+++ sh.sh
@@ -7,4 +7,4 @@ lerna publish
 nrm use taobao
 read -p 'version:' version
 # pnpm add vue-transfer-pagination@0.0."$version"
-# pnpm run serve
+# pnpm run dev
--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,8 @@ module.exports = {
   chainWebpack: (config) => {
     config.externals({
       ...config.get("externals"),
-      "@vue/composition-api": "@vue/composition-api",
-      vue: "vue",
+      // "@vue/composition-api": "@vue/composition-api",
+      // vue: "vue",
     });
   },
 };
